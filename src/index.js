eccde11ba72f035c978e03b180afdc72ad71cf10
ileristovski1 <62071112+ileristovski1@users.noreply.github.com>
2020-05-20 18:12:11 +0200

    commit to master

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello.js';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';
 
-ReactDOM.render(
-  <Hello greeting ={'Hello ' + 'React Ninja'}/>, document.getElementById('root')
+ReactDOM.render(<App/>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
